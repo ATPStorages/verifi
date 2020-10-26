bc2d65894a97b38bb2a126af4553a965ae866e22
unknown <900600masterplayer@gmail.com>
2020-10-26 12:36:03 -0500

    Add comments and url status check

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const express = require("express"),
     
     port = process.env.PORT || 3000;
 
+// Middleware //
+
 app.use(express.json());
-app.use(require("helmet")());
 app.use(require("compression")());
 
 app.use("/verify", require(__dirname + "/verify"));
 
+// Error Handler //
+
 app.use((err, _req, res, _next)=> {
     res.status(err.status || 500)
         .json({ error: err.message });
+});
+
+// Server //
+
+app.get("/", (req, res)=> {
+    res.end(`verifi active @ ${req.get("host")}!`)
 });
 
 app.listen(port, ()=> {
