bfb230d46f1eb56dddb4bc0fbd4d5b04afafb9be
unknown <900600masterplayer@gmail.com>
2020-10-27 00:36:11 -0500

    Update code - Add PUT

--- verify.js
+++ verify.js
@@ -9,28 +9,40 @@ router.get("/token/:rblxid", async(req, res, next)=> {
     
     let discid = await tokens.get(req.params.rblxid);
     if(!discid) return next({status: 404, message: "unable to find specified id in database"});
-    else res.json(discid);
+    else return res.json(discid);
 });
 
 router.post("/token", async(req, res, next)=> {
     if((isNaN(req.query.discid) || isNaN(req.query.rblxid)) || !req.query.tag)
         return next({status: 400, message: "provide discord and roblox id with tag (example#0000) in query"});
-    else if(tokens.get(req.query.rblxid))
+    else if(tokens.has(req.query.rblxid))
         return next({status: 403, message: "verification token already exists"});
 
-    tokens.set(req.query.rblxid, { id: req.query.discid, tag: req.query.tag });
+    tokens.set(req.query.rblxid, { id: req.query.discid, tag: req.query.tag, verified: false });
     res.json({"database_key": req.query.rblxid});
 });
 
-router.delete("/token/:rblxid", async(req, res, next)=> {
+router.put("/tokens/:rblxid(\\d+)?", async(req, res, next)=> {
+    if(isNaN(req.params.rblxid) || typeof(req.query.verified) != "string")
+        return next({status: 400, message: "provide valid roblox id and verified value"});
+
+    let discid = tokens.has(req.params.rblxid);
+    if(!discid) return next({status: 404, message: "unable to find specified id in database"});
+    else {
+        tokens.set(req.params.rblxid, req.query.verified == "true", "verified");
+        return res.json(await tokens.get(req.params.rblxid));
+    }
+});
+
+router.delete("/tokens/:rblxid(\\d+)?", async(req, res, next)=> {
     if(isNaN(req.params.rblxid))
         return next({status: 400, message: "provide valid roblox id"});
 
-    let discid = await tokens.get(req.params.rblxid);
+    let discid = tokens.has(req.params.rblxid);
     if(!discid) return next({status: 404, message: "unable to find specified id in database"});
     else {
         tokens.delete(req.params.rblxid);
-        res.json({"success": true})
+        return res.json({"success": true});
     }
 });
 
@@ -38,5 +50,4 @@ router.all("/test", (req, res)=> {
     res.end(`Controller "verify" - Router online, and using ${req.protocol}.`);
 });
 
-//commit test
 module.exports = router;
